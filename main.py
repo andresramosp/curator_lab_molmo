53534fa5b51bfffcd7bfabf0a10590a2a67ed48c
Andrés Ramos <andreschennai@gmail.com>
2025-02-22 07:19:17 +0000

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from transformers import AutoModelForCausalLM, AutoProcessor, GenerationConfig
 from PIL import Image
 import torch
 from io import BytesIO
+import requests
 
 # Iniciar la API
 app = FastAPI()
@@ -22,19 +23,27 @@ print("Modelo cargado.")
 @app.post("/generate")
 async def generate_text(image: UploadFile = File(...), prompt: str = "Describe this image."):
     # Leer la imagen
-    image_bytes = await image.read()
-    image = Image.open(BytesIO(image_bytes))
+    # image_bytes = await image.read()
+    # image = Image.open(BytesIO(image_bytes))
 
-    # Preprocesar la imagen
-    inputs = processor.process(images=[image], text=prompt)
+    # # Preprocesar la imagen
+    # inputs = processor.process(images=[image], text=prompt)
 
-    # Verificar si inputs contiene tensores
-    print("Inputs antes de conversión:", {k: type(v) for k, v in inputs.items()})
+    # # Verificar si inputs contiene tensores
+    # print("Inputs antes de conversión:", {k: type(v) for k, v in inputs.items()})
 
-    # Convertir listas en tensores y moverlos a la GPU
-    inputs = {k: torch.tensor(v).to(model.device).unsqueeze(0) if isinstance(v, list) else v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
+    # # Convertir listas en tensores y moverlos a la GPU
+    # inputs = {k: torch.tensor(v).to(model.device).unsqueeze(0) if isinstance(v, list) else v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
 
-    print("Inputs después de conversión:", {k: v.shape for k, v in inputs.items()})
+    # print("Inputs después de conversión:", {k: v.shape for k, v in inputs.items()})
+
+    inputs = processor.process(
+    images=[Image.open(requests.get("https://picsum.photos/id/237/536/354", stream=True).raw)],
+    text="Describe this image."
+)
+
+    # move inputs to the correct device and make a batch of size 1
+    inputs = {k: v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
 
     # Generación de texto
     with torch.autocast(device_type="cuda", enabled=True, dtype=torch.bfloat16):
