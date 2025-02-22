67f335f01f858692ebd1a5b92235283032bcf2a7
Andrés Ramos <andreschennai@gmail.com>
2025-02-22 09:01:01 +0000

    update

--- main.py
+++ main.py
@@ -35,20 +35,19 @@ torch.cuda.empty_cache()
 async def generate_text(image: UploadFile = File(...), prompt: str = "Describe this image."):
 
     inputs = processor.process(
-        images=[Image.open(requests.get("https://picsum.photos/id/237/536/354", stream=True).raw)],
-        text="Describe this image."
-    )
+    images=[Image.open(requests.get("https://picsum.photos/id/237/536/354", stream=True).raw)],
+    text="Describe this image."
+)
 
-    # move inputs to the correct device and make a batch of size 1
+# move inputs to the correct device and make a batch of size 1
     inputs = {k: v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
 
     # generate output; maximum 200 new tokens; stop generation when <|endoftext|> is generated
-    with torch.autocast(device_type="cuda", enabled=True, dtype=torch.bfloat16):
-        output = model.generate_from_batch(
-            inputs,
-            GenerationConfig(max_new_tokens=50, stop_strings="<|endoftext|>"),
-            tokenizer=processor.tokenizer
-        )
+    output = model.generate_from_batch(
+        inputs,
+        GenerationConfig(max_new_tokens=200, stop_strings="<|endoftext|>"),
+        tokenizer=processor.tokenizer
+    )
 
     # only get generated tokens; decode them to text
     generated_tokens = output[0,inputs['input_ids'].size(1):]
